9a7a26ae968fe03ddab8708479ec49e0c6f74c35
Johannes Wüller <johanneswueller@gmail.com>
2015-10-26 00:15:04 +0100

    first page version

---
+++ populate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#
+# This script pulls all gh-pages files from the build location.
+#
+
+if [ -z "$1" ];
+then
+    echo "Usage: $0 path/to/dist/gh-pages"
+    exit 1
+fi
+
+cp -rf "$1"/* .
