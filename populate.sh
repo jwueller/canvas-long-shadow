5b8e2c05347fbe2cae23365bd864b6d7e608afbb
Johannes Wüller <johanneswueller@gmail.com>
2015-10-29 19:35:21 +0100

    make update script more robust

--- populate.sh
+++ populate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e # fail on error
 
 #
 # This script pulls all gh-pages files from the build location.
@@ -10,4 +11,11 @@ then
     exit 1
 fi
 
-cp -rfv "$1"/* .
+ABSOLUTE_SRC_PATH=`realpath $1`
+DST_PATH=`dirname "$0"`
+
+pushd "$DST_PATH" > /dev/null
+find * -not -name ".gitignore" -not -name "populate.sh" -delete
+popd > /dev/null
+
+cp -rfv "$1"/* "$DST_PATH"
