a2c789f67ac7c756e0b8bf67a869d5729ade91ec
Johannes Wüller <johanneswueller@gmail.com>
2015-10-29 19:42:15 +0100

    clean up

--- populate.sh
+++ populate.sh
@@ -11,11 +11,14 @@ then
     exit 1
 fi
 
-ABSOLUTE_SRC_PATH=`realpath $1`
 DST_PATH=`dirname "$0"`
+SELF_NAME=`basename "$0"`
 
+# Clean up. Change the PWD to ensure that this script does not get deleted when
+# calling from somewhere else.
 pushd "$DST_PATH" > /dev/null
-find * -not -name ".gitignore" -not -name "populate.sh" -delete
+find * -not -name "$SELF_NAME" -delete
 popd > /dev/null
 
+# Get the latest stuff.
 cp -rfv "$1"/* "$DST_PATH"
